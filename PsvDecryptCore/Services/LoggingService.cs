39bcde9b34703d0f25d9f9d093eee6699add77c9
Hsu Still <341464@gmail.com>
2018-01-28 18:07:51 +0800

    Add safe navigation operator in logging to prevent null ref

--- PsvDecryptCore/Services/LoggingService.cs
+++ PsvDecryptCore/Services/LoggingService.cs
@@ -17,9 +17,9 @@ namespace PsvDecryptCore.Services
             .CreateLogger("Main");
 
         public void Log(LogLevel logLevel, string message) => _logger.Log(logLevel, 0, message, null,
-            (s, exception) => s.ToString());
+            (s, exception) => s?.ToString());
 
         public void LogException(LogLevel logLevel, Exception ex) => _logger.Log(logLevel, 0, string.Empty, ex,
-            (s, exception) => exception.ToString());
+            (s, exception) => exception?.ToString());
     }
 }
